b53967c42c700ac90d07ddb4988ae29f3f0b18ea
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-08-25 15:32:31 -0600

    Reorganize & modify page handlers/renderer

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -3,27 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Kevonosdiaz/Go-SideProjects/hello-world/pkg/handlers"
 )
 
 const PORT_NO = ":8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-
-}
-
 // main is the main app fn
 func main() {
 	// Handler function using an anonymous fn within it
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", PORT_NO))
 	// Start the web server, with port being 8080
 	_ = http.ListenAndServe(PORT_NO, nil)
+
 }
