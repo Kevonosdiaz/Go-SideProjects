cb73a332d0fb32168e28e01c584e77a7e7b411a1
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-08-25 16:00:33 -0600

    Change rendering method
    Switch to an approach that stores a cache of already loaded templates to avoid unnecessary disk checking

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -3,15 +3,49 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+// tc is a template cache, mapping the name to the template ptr
+var tc = make(map[string]*template.Template)
+
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if template exists in cache already
+	_, inMap := tc[t]
+
+	if !inMap {
+		// Create template and add it
+		err = createTemplateCache(t)
+		if err != nil {
+			fmt.Println(err)
+		}
+		log.Println("adding new template to cache")
+	} else {
+		log.Println("using existing template from cache")
 	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+
+}
+
+// createTemplateCache updates the cache with a new template
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+
+	return nil
 }
